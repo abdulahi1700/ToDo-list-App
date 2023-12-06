498b8a26c4e7aec159baa9e1c4245d69074c5c9f
unknown <abdulahihaji1504@gmail.com>
2023-12-06 21:38:25 +0300

    commit todo list app

---
+++ main.js
@@ -0,0 +1,38 @@
+const inputBox = document.getElementById('input-box');
+const Listcontainer = document.getElementById('list-container');
+const Button = document.getElementById('btn');
+
+Button.addEventListener('click', () => {
+  if (inputBox.value === '') {
+    alert('you must write something');
+  } else {
+    let li = document.createElement('li');
+    li.innerHTML = inputBox.value;
+    Listcontainer.appendChild(li);
+    let span = document.createElement('span');
+    span.innerHTML = '\u00d7';
+    li.appendChild(span);
+  }
+  inputBox.value = '';
+  SaveData();
+});
+
+Listcontainer.addEventListener('click', (e) => {
+  if (e.target.tagName === 'LI') {
+    e.target.classList.toggle('checked');
+    SaveData();
+  } else if (e.target.tagName === 'SPAN') {
+    e.target.parentElement.remove();
+    SaveData();
+  }
+  false;
+});
+
+function SaveData() {
+  localStorage.setItem('data', Listcontainer.innerHTML);
+}
+
+function showTask() {
+  Listcontainer.innerHTML = localStorage.getItem('data');
+}
+showTask();
